cc4ef345d14c32ef6aed58cd833fe4a0362d280a
Mashonne <himashcareem16@gmail.com>
2023-07-11 15:13:48 +0530

    final

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-
 export default function AuthLayout({
   children,
 }: {
-  childern: React.ReactNode;
+  children: React.ReactNode
 }) {
-  return <div className="flex items-center justify-center h-full w-full">
-    {children}
-    </div>;
-}
+  return (
+    <div className="flex items-center justify-center h-full w-full">
+      {children}
+    </div>
+  );
+};
