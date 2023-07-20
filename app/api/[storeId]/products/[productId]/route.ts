ea623786c06e6412bf9673810bcfc3d2e4a96a22
Mashonne <himashcareem16@gmail.com>
2023-07-21 01:52:00 +0530

    final

--- app/api/[storeId]/products/[productId]/route.ts
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -3,16 +3,6 @@ import { auth } from "@clerk/nextjs";
 
 import prismadb from "@/lib/prismadb";
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
-
-export async function OPTIONS() {
-  return NextResponse.json({}, { headers: corsHeaders });
-}
-
 export async function GET(
   req: Request,
   { params }: { params: { productId: string } }
@@ -180,9 +170,7 @@ export async function PATCH(
       },
     });
 
-    return NextResponse.json(product, {
-      headers: corsHeaders,
-    });
+    return NextResponse.json(product);
   } catch (error) {
     console.log("[PRODUCT_PATCH]", error);
     return new NextResponse("Internal error", { status: 500 });
