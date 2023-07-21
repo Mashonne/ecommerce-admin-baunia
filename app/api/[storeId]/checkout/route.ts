abe91fde3ff649a511de48ca27bf787fe4a023fc
Mashonne <himashcareem16@gmail.com>
2023-07-21 13:38:13 +0530

    correction

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 import { number } from "zod";
- 
+
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -19,9 +19,7 @@ export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
-  const items = await req.json();
-
-  const productIds = items.map((item: { id: string; }) => item.id)
+  const { productIds } = await req.json();
 
   if (!productIds || productIds.length === 0) {
     return new NextResponse("Product ids are required", { status: 400 });
@@ -30,29 +28,23 @@ export async function POST(
   const products = await prismadb.product.findMany({
     where: {
       id: {
-        in: productIds
-      }
-    }
+        in: productIds,
+      },
+    },
   });
-
-  const getQuantity = (id: string) => {
-    const qty = items.filter((product: { id: string, cartQuantity: number }) => product.id === id ? product.cartQuantity : 1);
-    const numberedQty = Number(qty)
-    return numberedQty
-  }
 
   const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
 
   products.forEach((product) => {
     line_items.push({
-      quantity: getQuantity(product.id),
+      quantity: 1,
       price_data: {
-        currency: 'USD',
+        currency: "USD",
         product_data: {
           name: product.name,
         },
-        unit_amount: product.price.toNumber() * 100
-      }
+        unit_amount: product.price.toNumber() * 100,
+      },
     });
   });
 
@@ -64,29 +56,32 @@ export async function POST(
         create: productIds.map((productId: string) => ({
           product: {
             connect: {
-              id: productId
-            }
-          }
-        }))
-      }
-    }
+              id: productId,
+            },
+          },
+        })),
+      },
+    },
   });
 
   const session = await stripe.checkout.sessions.create({
     line_items,
-    mode: 'payment',
-    billing_address_collection: 'required',
+    mode: "payment",
+    billing_address_collection: "required",
     phone_number_collection: {
       enabled: true,
     },
     success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
     cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
     metadata: {
-      orderId: order.id
+      orderId: order.id,
     },
   });
 
-  return NextResponse.json({ url: session.url }, {
-    headers: corsHeaders
-  });
-};
+  return NextResponse.json(
+    { url: session.url },
+    {
+      headers: corsHeaders,
+    }
+  );
+}
